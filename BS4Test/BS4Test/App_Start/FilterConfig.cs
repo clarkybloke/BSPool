11fd3a5b9f27b045f61d9a09c2939861fd930a47
Richard Clark <richardclark@docman.com>
2018-07-27 09:37:36 +0100

    Added

---
+++ BS4Test/BS4Test/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BS4Test
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
